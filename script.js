47f9444c0141d0ec8070ba1440ac2338e5b13c77
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-24 18:05:48 +0530

    calculate taxes

--- script.js
+++ script.js
@@ -86,3 +86,108 @@ const products = [
   },
 ];
 
+// Populate dessert cards with product information
+products.forEach(
+    ({ name, id, price, category }) => {
+      dessertCards.innerHTML += `
+        <div class="dessert-card">
+          <h2>${name}</h2>
+          <p class="dessert-price">$${price}</p>
+          <p class="product-category">Category: ${category}</p>
+          <button 
+            id="${id}" 
+            class="btn add-to-cart-btn">Add to cart
+          </button>
+        </div>
+      `;
+    }
+  );
+  
+  // ShoppingCart class to manage items in the cart
+  class ShoppingCart {
+    constructor() {
+      this.items = [];
+      this.total = 0;
+      this.taxRate = 8.25;
+    }
+   // Add item to the cart
+    addItem(id, products) {
+      const product = products.find((item) => item.id === id);
+      const { name, price } = product;
+      this.items.push(product);
+  
+      // Count occurrences of each product in the cart
+      const totalCountPerProduct = {};
+      this.items.forEach((dessert) => {
+        totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+      })
+  
+      const currentProductCount = totalCountPerProduct[product.id];
+      const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+  
+      currentProductCount > 1 
+        ? currentProductCountSpan.textContent = `${currentProductCount}x`
+        : productsContainer.innerHTML += `
+        <div id=dessert${id} class="product">
+          <p>
+            <span class="product-count" id=product-count-for-id${id}></span>${name}
+          </p>
+          <p>${price}</p>
+        </div>
+        `;
+    }
+  
+    // Get the total number of items in the cart
+    getCounts() {
+      return this.items.length;
+    }
+    // Clear all items from the cart
+    clearCart() {
+      if (!this.items.length) {
+        alert("Your shopping cart is already empty");
+        return;
+      }
+  
+      const isCartCleared = confirm(
+        "Are you sure you want to clear all items from your shopping cart?"
+      );
+  
+      if (isCartCleared) {
+        this.items = [];
+        this.total = 0;
+        productsContainer.innerHTML = "";
+        totalNumberOfItems.textContent = 0;
+        cartSubTotal.textContent = 0;
+        cartTaxes.textContent = 0;
+        cartTotal.textContent = 0;
+      }
+    }
+    // Calculate taxes based on the given amount
+    calculateTaxes(amount) {
+      return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+    }
+  
+    calculateTotal() {
+      const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+      const tax = this.calculateTaxes(subTotal);
+      this.total = subTotal + tax;
+      cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+      cartTaxes.textContent = `$${tax.toFixed(2)}`;
+      cartTotal.textContent = `$${this.total.toFixed(2)}`;
+      return this.total;
+    }
+  };
+  
+  const cart = new ShoppingCart();
+  const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+  
+  [...addToCartBtns].forEach(
+    (btn) => {
+      btn.addEventListener("click", (event) => {
+        cart.addItem(Number(event.target.id), products);
+        totalNumberOfItems.textContent = cart.getCounts();
+        cart.calculateTotal();
+      })
+    }
+  );
+ 
